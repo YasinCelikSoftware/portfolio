ecc32f157185f924230f15071c1fb0b34bfef86d
YasinCelikSoftware <yasin.celik4@outlook.com>
2023-08-28 22:19:47 +0300

    responsive bug fix

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -1,10 +1,20 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 import '../assets/css/Hamburger.css';
 
-function Hamburger({onClick}) {
+function Hamburger({onClick, checked}) {
+  const [isChecked, setChecked] = useState (checked);
+
+  const handleChange = e => {};
+
   return (
     <Fragment>
-      <input type="checkbox" id="nav_checkbox" onClick={() => onClick ()} />
+      <input
+        type="checkbox"
+        id="nav_checkbox"
+        onClick={() => onClick ()}
+        checked={checked}
+        onChange={handleChange}
+      />
       <label htmlFor="nav_checkbox" className="nav__toggle">
         <div className="bars" id="bar1" />
         <div className="bars" id="bar2" />
--- src/components/Navi.js
+++ src/components/Navi.js
@@ -6,25 +6,36 @@ import {Link} from 'react-router-dom';
 import DescriptionIcon from '@mui/icons-material/Description';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import CallOutlinedIcon from '@mui/icons-material/CallOutlined';
+import HomeIcon from '@mui/icons-material/Home';
 import ToggleSwitch from './ToggleSwitch';
 import Hamburger from './Hamburger';
 
 function Navi({changelanguage}) {
   const [isOpen, setIsOpen] = useState (false);
+  const [isHamburgerChecked, setHamburgerChecked] = useState (false);
 
-  const toggle = () => setIsOpen (!isOpen);
+  const toggle = () => {
+    setIsOpen (!isOpen);
+    setHamburgerChecked (!isHamburgerChecked);
+  };
 
   const {t} = useTranslation ();
 
   return (
     <div>
       <Navbar expand="md" className="mt-3 navi px-3">
-        <Link to="/" className="navbar-brand" href="/">{t ('navBrand')}</Link>
-        <Hamburger onClick={toggle} />
+        <Link to="/" className="navbar-brand">{t ('navBrand')}</Link>
+        <Hamburger onClick={toggle} checked={isHamburgerChecked} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="m-auto" navbar>
             <NavItem>
-              <Link className="nav-link" to="/my-cv">
+              <Link className="nav-link" to="/" onClick={toggle}>
+                <HomeIcon className="me-1 ms-2" />
+                {t ('home')}
+              </Link>
+            </NavItem>
+            <NavItem>
+              <Link className="nav-link" to="/my-cv" onClick={toggle}>
                 <DescriptionIcon className="me-1 ms-2" />
                 {t ('myCv')}
               </Link>
@@ -39,7 +50,7 @@ function Navi({changelanguage}) {
               </NavLink>
             </NavItem>
             <NavItem>
-              <Link className="nav-link" to="/contact">
+              <Link className="nav-link" to="/contact" onClick={toggle}>
                 <CallOutlinedIcon className="me-1 ms-2" />
                 {t ('contact')}
               </Link>
