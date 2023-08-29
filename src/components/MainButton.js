ac6c9b27e73c9bde9877c82ecf5edeee55b70322
YasinCelikSoftware <yasin.celik4@outlook.com>
2023-08-29 19:33:51 +0300

    disabled prop added to button

--- src/components/MainButton.js
+++ src/components/MainButton.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import '../assets/css/MainButton.css';
 
-function MainButton({children, type}) {
+function MainButton({children, type, disabled}) {
   return (
-    <button className="myButton" type={type}>
+    <button className="myButton" type={type} disabled={disabled}>
       <strong>{children}</strong>
       <div id="container-stars">
         <div id="stars" />
